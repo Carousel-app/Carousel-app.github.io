67ca00c86105535025491df45ed916467718b1e0
Carousel-app <carouselapp18@gmail.com>
2018-05-07 00:08:40 -0500

    Real DB

--- bd_connection.php
+++ bd_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-  $conn = new mysqli('localhost', 'root', '', 'carouseldb');
+  $conn = new mysqli('us-cdbr-sl-dfw-01.cleardb.net', 'b28bb6fa080be3', '9288b65a87d9367 ', 'users');
 
   if($conn->connect_error){
     echo $error = $conn->connect_error;
